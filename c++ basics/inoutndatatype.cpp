60f50b318e5f9f88007127c1301cbff4978e87b2
Abhishek Sharma <abhisheksharma46002@gmail.com>
2024-01-02 12:17:31 +0530

    setup and c++ basics

---
+++ c++ basics/inoutndatatype.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    
+    // std::cout<<"hello "<<std::endl;
+    // std::cout<<"Abhishek\n";
+    
+    // int x, y;
+    // cin>>x;
+    // cin>>y;
+    // cout<<"value of x: "<<x<<"and  y :"<<y;
+
+    //int 
+    int abhishek1=10;
+    long x=1000;
+    long long y=130000000;
+
+    //float and double
+    float f1=12.5;
+    double f2 = 13.5;
+
+    //string and getlin;
+    // string s ;
+    // cin>>s;
+    // string s2;
+    // cin>>s2;
+    // cout<<s<<s2;
+
+    // string str;
+    // getline(cin,str);
+    // cout<<str;
+
+    // char 
+    // char ch;
+    // cin>>ch;
+    // cout<<ch;
+
+    // -10^9 to 10^9 use int
+    // -10^12 to 10^12 use long int
+    // -10^18 to 10^18 use long long 
+
+
+    
+
+    return 0;
+}   
